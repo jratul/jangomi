c47de3b502484844431835cd5beeefc0cd863722
jratul <jratul@users.noreply.github.com>
2026-03-31 11:56:05 +0900

    fix: AGP 9.1.0 kotlin-android 플러그인 충돌 수정
    AGP 9.1.0이 내부적으로 kotlin-android를 자동 적용하므로
    명시적 kotlin-android 플러그인 선언 제거 (kotlin extension 중복 등록 오류 해결)
    kotlinOptions 블록도 제거 (compileOptions의 Java 11 설정으로 대체)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
@@ -37,10 +36,6 @@ android {
         isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = "11"
     }
 
     buildFeatures {
